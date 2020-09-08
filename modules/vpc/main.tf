a949ec3e7524c82df4ba6a87d065a889d45eda03
nmarchini <nick.marchini@slalom.com>
2020-09-08 21:24:39 +0200

    multiple changes, vpc, tags, pre-commit, EC2 block device

--- modules/vpc/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "scope" {
-  cidr_block           = var.cidr
-  instance_tenancy     = var.tenancy
-  enable_dns_support   = var.dns_support
-  enable_dns_hostnames = var.dns_hostnames
-  tags                 = var.tags
-}
